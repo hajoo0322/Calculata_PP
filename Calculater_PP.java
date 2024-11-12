a40d6fd74037e877f7f45569d1929d9d0bd2095c
afkorigin <gnstldgk@naver.com>
2024-11-12 11:35:21 +0900

    PP add

---
+++ Calculater_PP.java
@@ -0,0 +1,37 @@
+package calculater_sparta;
+
+import java.util.Scanner;
+
+public class Calculater_PP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int num1 = sc.nextInt();
+            if (num1 < 0) {
+                System.out.println("양의 정수를 입력하시오");
+                break;
+            }
+            String booho = sc.next();
+            int num2 = sc.nextInt();
+            if (num2 < 0) {
+                System.out.println("양의 정수를 입력하시오");
+                break;
+            }
+            if (booho.equals("+")) {
+                System.out.println(num1 + num2);
+                break;
+            } else if (booho.equals("-")) {
+                System.out.println(num1 - num2);
+                break;
+            } else if (booho.equals("*")) {
+                System.out.println(num1 * num2);
+                break;
+            } else if (booho.equals("/")) {
+                System.out.println(num1 / num2);
+                break;
+            }
+        }
+
+
+    }
+}
