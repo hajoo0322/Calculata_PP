aa13f71e6c4245c31513185e7b1fc9459a3dfd91
afkorigin <gnstldgk@naver.com>
2024-11-20 20:08:56 +0900

    static fix

--- repository/Database.java
+++ repository/Database.java
@@ -7,7 +7,7 @@ public class Database {
     private double number1;
     private double number2;
     private String sign;
-    private List<Double> datas = new ArrayList<>();
+    private static List<Double> datas = new ArrayList<>();
 
     public double getNumber1() {
         return number1;
